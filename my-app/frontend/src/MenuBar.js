be4708020d61b1e4b4cd544e74a7e62e4317ad63
Michael <michaelnguyen408@gmail.com>
2020-10-07 15:50:38 -0700

    Updated imports

--- my-app/frontend/src/MenuBar.js
+++ my-app/frontend/src/MenuBar.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu'
-import { Typography, Button, Grid, Box, Hidden, Icon, SwipeableDrawer, List, ListItem, ListItemText, CssBaseline, Divider, ListItemIcon } from '@material-ui/core';
+import { Typography, Button, Grid, Box, Hidden,  List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 import Drawer from '@material-ui/core/Drawer'
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 
