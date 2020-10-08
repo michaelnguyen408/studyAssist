40412094bac369c271955d2772c8e3016d94ec2c
Michael <michaelnguyen408@gmail.com>
2020-10-08 16:04:05 -0700

    Added link to Get Started button

--- my-app/frontend/src/components/Homepage.js
+++ my-app/frontend/src/components/Homepage.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { Typography,  createMuiTheme, responsiveFontSizes, ThemeProvider, Button } from '@material-ui/core';
 import DirectionsRunRoundedIcon from '@material-ui/icons/DirectionsRunRounded';
+import { Link } from 'react-router-dom';
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
@@ -21,6 +22,7 @@ const useStyles = makeStyles((theme) => ({
     color: 'white',
     '&:hover': {
       backgroundColor: 'black',
+      color: 'white'
     }
   },
 }));
@@ -45,7 +47,10 @@ export default function Homepage () {
             </Typography>
           </Grid>
           <Grid item>
-            <Button className={classes.button} variant='contained' disableRipple="true" disableElevation='true'>
+            <Button 
+              className={classes.button} 
+              component={Link} to='/login'
+              variant='contained' disableRipple="true" disableElevation='true'>
               Get Started
             </Button>
           </Grid>
